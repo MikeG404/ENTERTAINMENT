1a0b654f8d23fe17246c1ef2735eba7b4188a5b1
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-10-12 07:17:26 +0200

    Add route section trending

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
+import TrendingSection from './components/Trending/TrendingSection'
 import { MoviePage, SeriePage, BookmarkPage, HomePage } from './pages/index'
 import './App.scss'
 
@@ -9,9 +10,11 @@ function App() {
 		<>
 			<Routes>
 				<Route element={<MainLayout />}>
-					<Route index element={<HomePage />} />
-					<Route path="/movies" element={<MoviePage />}></Route>
-					<Route path="/series" element={<SeriePage />}></Route>
+					<Route element={<TrendingSection />}>
+						<Route index element={<HomePage />} />
+						<Route path="/movies" element={<MoviePage />}></Route>
+						<Route path="/series" element={<SeriePage />}></Route>
+					</Route>
 					<Route path="/bookmarks" element={<BookmarkPage />}></Route>
 				</Route>
 			</Routes>
---
+++ src/components/Trending/TrendingSection.jsx
@@ -0,0 +1,10 @@
+const TrendingSection = () => {
+
+    return (
+        <section>
+            <h1>Trending</h1>
+        </section>
+    )
+}
+
+export default TrendingSection;
