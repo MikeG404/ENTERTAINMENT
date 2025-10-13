ddaa3d9b2db2df2f86802ac821330a515282e2ab
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-10-13 03:51:22 +0200

    Add information layout on Trending Card

--- src/components/Trending/TrendingCard/TrendingCard.jsx
+++ src/components/Trending/TrendingCard/TrendingCard.jsx
@@ -1,9 +1,23 @@
 import styles from "./trendingcard.module.scss"
+import { IconCategoryMovie } from "../../../assets/index"
 
 const TrendingCard = ({ image, title }) => {
 
     return (
         <div className={styles.itemContainer}>
+            <div className={styles.cardInfos}>
+                <div className={styles.upperCardInfos}>
+                    <p>2019</p>
+                    <span>.</span>
+                    <div className={styles.category}>
+                        <img src={IconCategoryMovie} alt="" />
+                        <p>Movie</p>
+                    </div>
+                    <span>.</span>
+                    <p>PG</p>
+                </div>
+                <p>{ title }</p>
+            </div>
             <img className={styles.image} src={image} alt={title} loading="lazy"/>
         </div>
     )
