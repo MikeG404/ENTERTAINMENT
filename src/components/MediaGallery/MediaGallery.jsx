c11aa7ff04d3eabd02a5574027ea91a08a96e097
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-10-16 10:54:52 +0200

    Responsive padding on aall components

--- src/components/MediaGallery/MediaGallery.jsx
+++ src/components/MediaGallery/MediaGallery.jsx
@@ -3,8 +3,8 @@ import MediaCard from "./MediaCard/MediaCard";
 
 const MediaGallery = ({movies, sectionTitle}) => {
     return (
-        <section>
-            <h2 className="text-preset-m1" style={{paddingBlock: "var(--spacing-200)", paddingInline: "var(--spacing-200)"}}>{sectionTitle}</h2>
+        <section className={styles.mediaGallerySection}>
+            <h2 className="text-preset-m1">{sectionTitle}</h2>
             <div className={styles.mediaGalleryContainer}>
                 {movies.map((movie) => {
                     return <MediaCard
